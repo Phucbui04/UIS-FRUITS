377ec1d7568e221d306d1151101d840d0823ebee
Lai Thanh Hoa <laithanhhoa16092004@gmail.com>
2024-09-28 16:04:17 +0700

    template blade layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+Route::get('/product/id', [ProductController::class, 'detail'])->name('product.detail');
+
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
