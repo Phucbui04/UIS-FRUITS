ca48f96c25e39a45c683010ef792456620f1ca86
duynecon <nguyendangduy0812@gmail.com>
2024-10-07 11:26:42 +0700

    duy ne con

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,12 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
+    // Hiển thị giỏ hàng
     public function index()
     {
-        return view('pages.cart');
+        $cart = Session::get('cart', []);
+        $totalPrice = array_reduce($cart, function ($carry, $item) {
+            return $carry + ($item['price'] * $item['quantity']);
+        }, 0);
+
+        return view('pages.cart', compact('cart', 'totalPrice'));
+    }
+
+    // Thêm sản phẩm vào giỏ hàng
+    public function addToCart(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $cart = Session::get('cart', []);
+
+        if (isset($cart[$id])) {
+            $cart[$id]['quantity'] += $request->quantity;
+        } else {
+            $cart[$id] = [
+                'name' => $product->name,
+                'price' => $product->price,
+                'image' => $product->image, 
+                'quantity' => $request->quantity
+            ];
+        }
+
+        Session::put('cart', $cart);
+
+        return redirect()->route('cart.index')->with('success', 'Sản phẩm đã được thêm vào giỏ hàng');
+
+    }
+
+    // Cập nhật số lượng giỏ hàng
+    public function updateCart(Request $request, $id)
+    {
+        $cart = Session::get('cart', []);
+
+        if (isset($cart[$id])) {
+            $cart[$id]['quantity'] = $request->quantity;
+
+            Session::put('cart', $cart);
+        }
+
+        return response()->json(['success' => true, 'totalPrice' => $this->calculateTotal()]);
+    }
+
+    // Xóa sản phẩm khỏi giỏ hàng
+    public function delete($id)
+    {
+        $cart = Session::get('cart', []);
+        unset($cart[$id]);
+
+        Session::put('cart', $cart);
+
+        return response()->json(['success' => true, 'message' => 'Sản phẩm đã xóa thành công', 'totalPrice' => $this->calculateTotal()]);    }
+
+    // Tính tổng tiền
+    private function calculateTotal()
+    {
+        $cart = Session::get('cart', []);
+        return array_reduce($cart, function ($carry, $item) {
+            return $carry + ($item['price'] * $item['quantity']);
+        }, 0);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,24 @@ Route::get('/', [HomeController::class, 'index'])->name('home.index');
 Route::get('/product', [ProductController::class, 'index'])->name('product.index');
 Route::get('/product/{id}', [ProductController::class, 'detail'])->name('product.detail');
 
+
+
+// Hiển thị giỏ hàng
 Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::post('/cart/add/{id}', [CartController::class, 'addToCart'])->name('cart.add');
+Route::put('/cart/update/{id}', [CartController::class, 'updateCart'])->name('cart.update');
+Route::delete('/cart/delete/{id}', [CartController::class, 'delete'])->name('cart.delete');
+
+
+
+    
 Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
 
 
 
-Route::prefix('admin')->group(function () {
+/* Route::prefix('admin')->group(function () {
     Route::get('/', DashboardController::class);
     Route::resource('products', AdminProductController::class);
     Route::resource('categories', AdminCategoryController::class);
     Route::resource('users', AdminUserController::class);
-});
+}); */
